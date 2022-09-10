a3448ac5b91d391bcd0fe0f3e632c0508b39e37f
andresparrab <andres.parrab@gmail.com>
2022-09-10 23:17:34 +0200

    saturaday 72

--- index.ts
+++ index.ts
@@ -194,7 +194,7 @@ function getData(AllpushEvents)
     };
     return dataarray2;
 }
-// function populate(message:string, reponame : string, sha: string) adadsf
+// function populate(message:string, reponame : string, sha: string) adad
 // function populate(payload: any): CommitInfo
 // {
 //   return {
