87c830f21afbbefbeb1a0043368233ca209f2a0a
andresparrab <andres.parrab@gmail.com>
2022-09-10 20:40:17 +0200

    saturaday 39

--- index.ts
+++ index.ts
@@ -266,7 +266,7 @@ const updateReadmeFile = async (line: string): Promise<boolean> => {
 
   readmeFileLines.splice(
     startI + 1,
-    startI + 1 === endI ? 2 : endI - startI - 1,
+    startI + 1 === endI ? 4 : endI - startI - 1,
     line
   
   );
